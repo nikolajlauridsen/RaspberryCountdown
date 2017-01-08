541e7f8415903c221aaccbf7b1794ef42ea68d44
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-01-08 12:48:57 +0100

    Pomodorofy

--- CountDownTimer.py
+++ CountDownTimer.py
@@ -1,52 +1,36 @@
 import time
 
 import RPi.GPIO as GPIO
-from physical.notifier import Notifier
 
+from _timers.pomodoro import PomodoroTimer
+from physical.notifier import Notifier
 from physical import LCD_driver as lcdDriver
-from _timers.countdown import CountDown
 
 # Button shorthands
+buttons = {
+    'start': 23,
+    'stop': 24
+}
 start_bth = 23
 stop_btn = 24
 
 # Set up channels
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(start_bth, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(stop_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(start_bth, GPIO.RISING)
-GPIO.add_event_detect(stop_btn, GPIO.RISING)
+GPIO.setup(buttons['start'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttons['stop'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(buttons['start'], GPIO.RISING)
+GPIO.add_event_detect(buttons['stop'], GPIO.RISING)
 
 # Initialize objects
-timer = CountDown()
 screen = lcdDriver.lcd()
 notifier = Notifier(22, 27, 17)
+pomodoro = PomodoroTimer(screen, notifier, buttons)
 
-# Update LCD and wait for input TODO: do this with physical buttons
-screen.lcd_display_string('Waiting'.center(16, '~'), 1)
-screen.lcd_display_string('Input duration'.center(16, '~'), 2)
-duration = int(input('Input duraion: '))
-timer.set_duration(duration)
-screen.lcd_display_string('Press start'.center(16, '~'), 1)
-screen.lcd_display_string(timer.get_remaining_string().center(16, ' '), 2)
+pomodoro.main()
 
-# Main loop
-while not timer.check_expired():
-    # Detect events
-    if GPIO.event_detected(start_bth):
-        if timer.paused:
-            timer.start()
-            screen.lcd_display_string('Running'.center(16, '~'), 1)
-        else:
-            timer.pause()
-            screen.lcd_display_string('Paused'.center(16, '~'), 1)
-    # Update time remaining row on LCD 
-    screen.lcd_display_string(timer.get_remaining_string().center(16, ' '), 2)
-    # Sleep for a little while, no reason to overstress the CPU
-    time.sleep(0.2)
-
-# Timer finished, notify via blinks
-screen.lcd_display_string('Timer finished'.center(16, '~'), 1)
+# Program finished
+screen.lcd_clear()
+screen.lcd_display_string('Program ended'.center(16, '~'), 1)
 notifier.blink()
 # Cleanup
 notifier.clean()
---
+++ _timers/pomodoro.py
@@ -0,0 +1,77 @@
+import RPi.GPIO as GPIO
+import time
+from .countdown import CountDown
+
+class PomodoroTimer(CountDown):
+    def __init__(self, screen, notifier, buttons, study_length=25,
+                 break_length=5):
+        # Inherit from countdown timer
+        super().__init__()
+
+        self.screen = screen
+        self.buttons = buttons
+        self.notify = notifier
+        self.study_t = study_length
+        self.short_break = break_length
+
+        self.cycle = 0
+        self.long_break = 20
+        self.pomodoro_running = True
+        self.next_cycle = True
+
+    def run_timer(self, duration):
+        self.set_duration(duration)
+        self.start()
+        while not self.check_expired():
+            # Detect events
+            if GPIO.event_detected(self.buttons['start']):
+                if self.paused:
+                    self.start()
+                    self.screen.lcd_display_string('Session started '
+                                                   .center(16, ' '), 1)
+                else:
+                    self.pause()
+                    self.screen.lcd_display_string('Paused'.center(16, ' '), 1)
+            elif GPIO.event_detected(self.buttons['stop']):
+                # Stop the timer, return false, ie, no new cycle
+                return False
+
+            # Update time remaining on lcd
+            self.screen.lcd_display_string(self.get_remaining_string()
+                                           .center(16, ' '), 2)
+            # Sleep
+            time.sleep(0.2)
+
+        # cycle finished, blink and start new
+        self.notify.blink()
+        return True
+
+    def run_session(self):
+
+        while self.next_cycle:
+            if self.cycle % 3 != 0:
+                # TODO: convert seconds to minutes (seconds works well for dev)
+                self.next_cycle = self.run_timer(self.study_t)
+                self.next_cycle = self.run_timer(self.short_break)
+                self.cycle += 1
+            else:
+                self.next_cycle = self.run_timer(self.study_t)
+                self.next_cycle = self.run_timer(self.long_break)
+
+        # Session over TODO: make calendar event
+        print('Session finished')
+
+    def main(self):
+        while self.pomodoro_running:
+            self.screen.lcd_display_string('Pomodoro Timer'.center(16, ' '), 1)
+            self.screen.lcd_display_string('Press start'.center(16, ' '), 2)
+
+            if GPIO.event_detected(self.buttons['start']):
+                print('session')
+                self.run_session()
+
+            elif GPIO.event_detected(self.buttons['stop']):
+                print('Pomodoro stopped')
+                break
+
+        time.sleep(0.2)
