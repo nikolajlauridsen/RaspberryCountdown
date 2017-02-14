92c997ad0fd9ad45f9a182e1f87e8fec9274767f
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-02-14 18:47:18 +0100

    Comment activitytracker

--- TimeBuddy/timers/activitytracker.py
+++ TimeBuddy/timers/activitytracker.py
@@ -11,12 +11,14 @@ class ActivityTracker(StopWatch):
         super().__init__(screen, notifier, buttons)
         self.api_handler = ApiHandler()
         self.calendar = EventCreator()
-        self.tracker_start = None
+        self.tracker_start = None   # Start time for the tracker
 
     def __repr__(self):
         return """Activity Tracker"""
 
     def finish_tracker(self, activity):
+        """Finish a tracker session, saving it to the api database and
+        creating a google calendar event, reset the stopwatch as well"""
         end = time.time()
         duration = self.get_elapsed()
         self.screen.lcd_display_string('Saving event'.center(16, ' '), 2)
@@ -28,8 +30,10 @@ class ActivityTracker(StopWatch):
         self.calendar.create_event(summary,
                                    self.tracker_start, end,
                                    description=description)
+        self.reset()
 
     def reset(self):
+        """Reset the stopwatch"""
         self.screen.lcd_display_string("Stopped".center(16), 1)
         self.screen.lcd_display_string(" " * 16, 2)
         self.elapsed = 0
@@ -39,6 +43,7 @@ class ActivityTracker(StopWatch):
         self.start_time = None
 
     def run_tracker(self, activity):
+        """Run a tracker session, takes the name of the activity as a string"""
         # update screen
         if self.running:
             self.screen.lcd_display_string(activity.center(16), 1)
@@ -48,45 +53,64 @@ class ActivityTracker(StopWatch):
 
         # Tracker loop
         while True:
+            # Update the screen
             self.screen.lcd_display_string(self.get_elapsed_string().center(16, ' '), 2)
+
             if GPIO.event_detected(self.buttons['start']):
+
                 if self.start_time:
+                    # If the timer has been started, just toggle pause
                     self.toggle_pause(msg=activity['name'])
                 else:
+                    # If it hasn't, set the tracking session start time
                     self.tracker_start = time.time()
+                    # And then start the clock
                     self.start(msg=activity['name'])
 
             elif GPIO.event_detected(self.buttons['stop']):
+                # Finish the tracker, creating events and whatnot.
                 self.finish_tracker(activity["name"])
-                self.reset()
+                # Stop the tracking session
                 break
 
     def main(self):
+        """Main program for the activity tracker,
+        will let the user pick an activity, and then runs a tracker with
+        said ability"""
+
+        # Fetch activities from API
         activities = self.api_handler.get_activities('active')
 
+        # Cursor for pointing at an activity
         cursor = 0
         while True:
+            # Update the screen
             self.screen.lcd_display_string("Choose activity", 1)
             self.screen.lcd_display_string(activities[cursor]["name"].center(16, ' '), 2)
 
             if GPIO.event_detected(self.buttons['start']):
                 print('Starting activity tracker')
+                # Run the selected activity
                 self.run_tracker(activities[cursor])
+                # Refresh activities list
                 activities = self.api_handler.get_activities('active')
 
             elif GPIO.event_detected(self.buttons['forward']):
+                # Move the cursor one up, unless it's at the edge, then reset
                 if cursor < len(activities) - 1:
                     cursor += 1
                 else:
                     cursor = 0
 
             elif GPIO.event_detected(self.buttons['back']):
+                # Move the cursor one down, unless at..blah blah
                 if cursor > 0:
                     cursor -= 1
                 else:
                     cursor = len(activities)-1
 
             elif GPIO.event_detected(self.buttons['stop']):
+                # Stop the main loop
                 break
 
             time.sleep(0.2)
